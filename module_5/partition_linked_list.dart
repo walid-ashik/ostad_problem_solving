33bea12428249c63fab34e35a33d03b16ddaa800
walid-ashik <walid.al.ashik@gmail.com>
2024-01-01 13:02:08 +0600

    add partitioned linked list

--- module_5/partition_linked_list.dart
+++ module_5/partition_linked_list.dart
@@ -7,27 +7,84 @@ class ListNode {
 }
 
 void main(List<String> args) {
-  (ListNode, int) input = getHeadNodeAndSeparator();
+  String rangeString = stdin.readLineSync() ?? '';
+  String inputListString = stdin.readLineSync() ?? '';
+  final separatorString = stdin.readLineSync() ?? '';
 
-  final head = input.$1;
+  if (rangeString == '' || inputListString == '' || separatorString == '') {
+    return;
+  }
+
+  final separator = int.tryParse(separatorString);
+  if (separator == null) return;
+  final list = inputListString.split(' ');
+
+  ListNode? c, head;
+
+  for (int i = 0; i < list.length; i++) {
+    int? data = int.tryParse(list[i]);
+    if (data == null) return;
+    ListNode newNode = ListNode(data);
+
+    if (head == null) {
+      head = newNode;
+      c = head;
+    } else {
+      c!.next = newNode;
+      c = c.next;
+    }
+  }
 
   ListNode? currentNode = head;
+  ListNode? greaterNode, lowerNode, currentGreaterNode, currentLowerNode;
 
   while (currentNode != null) {
-    print(currentNode.val);
+    final newNode = ListNode(currentNode.val);
+
+    if (newNode.val < separator) {
+      if (lowerNode == null) {
+        lowerNode = newNode;
+        currentLowerNode = lowerNode;
+      } else {
+        currentLowerNode!.next = newNode;
+        currentLowerNode = currentLowerNode.next;
+      }
+    } else {
+      if (greaterNode == null) {
+        greaterNode = newNode;
+        currentGreaterNode = greaterNode;
+      } else {
+        currentGreaterNode!.next = newNode;
+        currentGreaterNode = currentGreaterNode.next;
+      }
+    }
     currentNode = currentNode.next;
   }
+
+  if (lowerNode != null) {
+    currentLowerNode?.next = greaterNode;
+  } else {
+    lowerNode = greaterNode;
+  }
+
+  ListNode? finalNode = lowerNode;
+  final buffer = StringBuffer();
+
+  while (finalNode != null) {
+    buffer.write('${finalNode.val} ');
+    finalNode = finalNode.next;
+  }
+  print(buffer.toString());
 }
 
-(ListNode head, int separator) getHeadNodeAndSeparator() {
-  int listRange = int.parse(stdin.readLineSync() ?? '0');
+/*
+ListNode getHeadNodeAndSeparator() {
+  String rangeString = stdin.readLineSync() ?? '';
   String inputListString = stdin.readLineSync() ?? '';
-  int seperator = int.parse(stdin.readLineSync() ?? '0');
+  separator = int.parse(stdin.readLineSync() ?? '0');
   final list = inputListString.split(' ');
 
   ListNode? currentNode, head;
-
-  print('creating nodes');
 
   for (int i = 0; i < list.length; i++) {
     int data = int.parse(list[i]);
@@ -41,5 +98,6 @@ void main(List<String> args) {
       currentNode = currentNode.next;
     }
   }
-  return (head!, seperator);
+  return head!;
 }
+*/
